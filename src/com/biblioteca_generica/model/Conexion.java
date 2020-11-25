2b00eb52a49256cd05f482ccf087ee9b241c7efd
kha-tharsis <dmarchantzuniga@gmail.com>
2020-11-25 11:51:26 -0300

    Agregada la clase conexion y libreria para BD

---
+++ src/com/biblioteca_generica/model/Conexion.java
@@ -0,0 +1,24 @@
+package prueba.dm.cl.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private Connection con;
+
+    public Conexion(String host,int port,String db,String user,String pass) {
+        String url = String.format("jdbc:mariadb://%s:%d/%s",host,port,db);
+        try {
+            con = DriverManager.getConnection(url,user,pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getCon(){
+        return con;
+    }
+
+}
